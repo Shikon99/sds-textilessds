80b2f67dc87b57d13b69e015d8485c21e0e326b6
Shikon99 <interrnationalgamerz652@gmail.com>
2026-06-12 23:25:58 +0600

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,7 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-export function createClient() {
+export const createClient = async () => {
+  const { createBrowserClient } = await import('@supabase/ssr');
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  )
-}
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+};
